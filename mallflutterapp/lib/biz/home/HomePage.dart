befa402f128418080372a895786e7fcfcdb75523
李志 <lizhid@youyou.com>
2019-06-13 20:55:34 +0800

    商城首页界面编写

--- mallflutterapp/lib/biz/home/HomePage.dart
+++ mallflutterapp/lib/biz/home/HomePage.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
@@ -50,8 +51,20 @@ class HomePageState extends State<HomePage> {
     "http://macro-oss.oss-cn-shenzhen.aliyuncs.com/mall/images/20181113/car_ad2.jpg"
   ];
 
+  /// banner控件列表
+  List<Widget> _bannerItemList = new List();
+
   /// 图片数量
   int _imageCount = 0;
+
+  /// pageView滚动控制器
+  PageController pageController = new PageController();
+
+  /// 定时器
+  Timer _timer;
+
+  /// 当前位置
+  int _index = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -59,47 +72,148 @@ class HomePageState extends State<HomePage> {
     return new Scaffold(
       body: Column(
         children: <Widget>[
+          _createTileBar(),
           _createPageView(),
-          TextField(
-              controller: _phoneController,
-              keyboardType: TextInputType.phone,
-              decoration: InputDecoration(
-                  contentPadding: EdgeInsets.all(10), labelText: "请输入手机号码")),
-          RaisedButton(
-              child: new Text('请求数据'),
-              onPressed: () {
-                if (_phoneController.text == null ||
-                    _phoneController.text.length < 11) {
-                  MallToast.showToast("请输入正确的手机号码");
-                  return;
-                }
+          _createOperateMenu(),
+          _crateLine(Colors.grey, 400),
+          _createBroadcastWidget(),
+        ],
+      ),
+    );
+  }
 
-                MobileAddressReqEntity entity = new MobileAddressReqEntity();
-                entity.phone = _phoneController.text;
-                RequestManager.httpRequest<MobileAddressRespEntity>(
-                    RequestTypeEnum.NET, true, ApiHost.phone_address_query,
-                    param: entity,
-                    methodType: RequestMethodEnum.GET, successCallback:
-                        (BaseResponseEntity<MobileAddressRespEntity> data) {
-                  if (data != null) {
-                    setState(() {
-                      _testEntity = data.result;
-                    });
-                    print(data.toJson().toString());
-                  }
-                }, errorCallBack: (MallException e) {
-                  print(e);
-                });
-              }),
-          RaisedButton(
-            child: new Text("微信精选"),
-            onPressed: () {},
+  @override
+  void initState() {
+    super.initState();
+    initBannerItem();
+    restartTimer();
+  }
+
+  /// 开始倒计时
+  /// @return void
+  /// @author lizhid
+  /// @modify
+  /// @date 2019/6/13 20:16
+  void restartTimer() {
+    clearTimer();
+    _timer =
+        new Timer.periodic(new Duration(milliseconds: 2000), (Timer timer) {
+      if (pageController.positions.isNotEmpty) {
+        _index = pageController.page.round() + 1;
+        pageController.animateToPage(_index,
+            duration: new Duration(milliseconds: 400), curve: Curves.linear);
+        setState(() {});
+      }
+    });
+  }
+
+  /// 清空计时器
+  /// @return void
+  /// @author lizhid
+  /// @modify
+  /// @date 2019/6/13 20:09
+  void clearTimer() {
+    if (_timer != null) {
+      _timer.cancel();
+      _timer = null;
+    }
+  }
+
+  /// 初始化 banner条目
+  /// @return void
+  /// @author lizhid
+  /// @modify
+  /// @date 2019/6/13 19:40
+  void initBannerItem() {
+    if (_imageList == null) {
+      return;
+    }
+    for (int i = 0; i < _imageList.length; i++) {
+      _bannerItemList.add(_createPageViewItem(i));
+    }
+  }
+
+  /// 划线
+  /// @param color 颜色
+  /// @param colorValue 颜色值
+  /// @return Widget
+  /// @author lizhid
+  /// @modify
+  /// @date 2019/6/13 17:32
+  Widget _crateLine(MaterialColor color, int colorValue) {
+    return Container(color: color[colorValue], height: 1);
+  }
+
+  /// 创建顶部状态栏
+  /// @return Widget
+  /// @author lizhid
+  /// @modify
+  /// @date 2019/6/13 15:23
+  Widget _createTileBar() {
+    return Container(
+      height: 70,
+      padding: EdgeInsets.only(top: 20),
+      child: Stack(
+        children: <Widget>[
+          Container(
+              padding: EdgeInsets.only(left: 10),
+              child: GestureDetector(
+                  child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Stack(children: <Widget>[
+                        Image.asset(
+                          "assets/images/icon_msg.png",
+                          width: 20,
+                          height: 20,
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(left: 12),
+                          width: 10,
+                          height: 10,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle, color: Colors.red),
+                        )
+                      ])),
+                  onTap: () {
+                    MallToast.showToast("点击了消息");
+                  })),
+          Center(
+            child:
+                Text("首页", style: TextStyle(color: Colors.black, fontSize: 20)),
           ),
-          Text("号码：${_testEntity.mobileNumber}"),
-          Text("运营商：${_testEntity.operator}"),
-          Text("地址：${_testEntity.province}${_testEntity.city}"),
-          Text("cityCode：${_testEntity.cityCode}"),
-          Text("zipCode：${_testEntity.zipCode}")
+          Align(
+              alignment: Alignment.centerRight,
+              child: Container(
+                  padding: EdgeInsets.only(right: 10),
+                  width: 60,
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                          padding: EdgeInsets.only(right: 10),
+                          child: GestureDetector(
+                              child: Image.asset(
+                                "assets/images/icon_search.png",
+                                width: 20,
+                                height: 20,
+                              ),
+                              onTap: () {
+                                MallToast.showToast("点击了搜索");
+                              })),
+                      GestureDetector(
+                          child: Stack(
+                            children: <Widget>[
+                              Image.asset(
+                                "assets/images/icon_car.png",
+                                width: 20,
+                                height: 20,
+                              )
+                            ],
+                          ),
+                          onTap: () {
+                            MallToast.showToast("点击了购物车");
+                          })
+                    ],
+                  )))
         ],
       ),
     );
@@ -113,10 +227,23 @@ class HomePageState extends State<HomePage> {
   Widget _createPageView() {
     return Container(
         height: 200,
-        child: PageView.custom(
-            childrenDelegate: SliverChildBuilderDelegate((context, index) {
-          return _createPageViewItem(index);
-        }, childCount: _imageList.length)));
+        child: Stack(children: <Widget>[
+          PageView.builder(
+              itemBuilder: (context, index) {
+                return _bannerItemList[index % _bannerItemList.length];
+              },
+              controller: pageController,
+              physics: const PageScrollPhysics(
+                  parent: const ClampingScrollPhysics()),
+              itemCount: 0x7fffffff,
+              onPageChanged: (index) {
+                _index = index;
+              }),
+          Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                  padding: EdgeInsets.only(bottom: 10), child: Text("占位")))
+        ]));
   }
 
   /// 创建条目
@@ -126,9 +253,119 @@ class HomePageState extends State<HomePage> {
   /// @modify
   /// @date 2019/5/30 11:53
   Widget _createPageViewItem(int index) {
-    return Image.network(
-      _imageList[index],
-      fit: BoxFit.cover,
+    return GestureDetector(
+      child: Image.network(
+        _imageList[index],
+        fit: BoxFit.cover,
+      ),
+      onTapDown: (TapDownDetails details) {},
+      onTap: () {
+        MallToast.showToast("点击了" + index.toString());
+      },
+      onTapUp: (TapUpDetails details) {},
     );
+  }
+
+  /// 操作菜单
+  /// @return Widget
+  /// @author lizhid
+  /// @modify
+  /// @date 2019/6/13 17:25
+  Widget _createOperateMenu() {
+    double itemWidth = 90;
+    return Container(
+      child: Row(
+        children: <Widget>[
+          Container(
+              width: itemWidth,
+              child: GestureDetector(
+                child: Column(
+                  children: <Widget>[
+                    Image.asset(
+                      "assets/images/icon_ht.png",
+                      width: 25,
+                      height: 25,
+                    ),
+                    Text("话题")
+                  ],
+                ),
+                onTap: () {
+                  MallToast.showToast("话题");
+                },
+              )),
+          Container(
+              width: itemWidth,
+              child: GestureDetector(
+                child: Column(
+                  children: <Widget>[
+                    Image.asset(
+                      "assets/images/icon_youxuan.png",
+                      width: 25,
+                      height: 25,
+                    ),
+                    Text("优选")
+                  ],
+                ),
+                onTap: () {
+                  MallToast.showToast("优选");
+                },
+              )),
+          Container(
+              width: itemWidth,
+              child: GestureDetector(
+                child: Column(
+                  children: <Widget>[
+                    Image.asset(
+                      "assets/images/icon_tehui.png",
+                      width: 25,
+                      height: 25,
+                    ),
+                    Text("特惠")
+                  ],
+                ),
+                onTap: () {
+                  MallToast.showToast("特惠");
+                },
+              )),
+          Container(
+              width: itemWidth,
+              child: GestureDetector(
+                child: Column(
+                  children: <Widget>[
+                    Image.asset(
+                      "assets/images/icon_sign.png",
+                      width: 25,
+                      height: 25,
+                    ),
+                    Text("签到")
+                  ],
+                ),
+                onTap: () {
+                  MallToast.showToast("签到");
+                },
+              ))
+        ],
+      ),
+      margin: EdgeInsets.only(top: 10, bottom: 10),
+    );
+  }
+
+  /// 创建广播消息
+  /// @return Widget
+  /// @author lizhid
+  /// @modify
+  /// @date 2019/6/13 17:26
+  Widget _createBroadcastWidget() {
+    return Container(
+        margin: EdgeInsets.only(top: 10),
+        child: Stack(children: <Widget>[
+          Align(
+              alignment: Alignment.centerLeft,
+              child: Container(
+                margin: EdgeInsets.only(left: 20),
+                child: Image.asset("assets/images/icon_broadcast.png"),
+              )),
+          Center(child: Text("广播通知"))
+        ]));
   }
 }
